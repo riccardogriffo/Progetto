8ba98331f788408e45281f5d7a2c7c4faf2c3614
riccardogriffo <riccardo.griffo@mail.polimi.it>
2020-10-30 11:58:30 +0000

    Aggiunta di main

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (){
+  printf ("inserici due numeri:");
+  float a;
+  float b;
+  scanf("%f",&a);
+  scanf("%f",&b);
+  printf ("la somma dei due numeri è %f \n",a+b);
+  return 0;
+}
